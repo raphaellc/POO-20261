7b2940870d88bfe9ae1ecd9cacafa80cd0281da2
kevingiediel <kevingiediel@gmail.com>
2026-03-06 21:27:28 -0300

    Create Pessoa.h

---
+++ kevin_giediel/Pessoa.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string> 
+
+using namespace std;
+
+class Pessoa {
+    private:
+    string nome;
+    int idade;
+
+    public:
+    Pessoa();
+    void definirNome(string n);
+    string dizerSeuNome();
+    void definirIdade(int i);
+    int dizerSuaIdade();
+
+}
