21c57ed2e276bcc354b5a8e62ac919035f00ff46
Niwlek97 <kelwincleyton@gmail.com>
2026-03-20 19:37:58 -0300

    Update Personagem.cpp

--- aula03/Personagem.cpp
+++ aula03/Personagem.cpp
@@ -1,34 +1,51 @@
+
 #include "Personagem.h"
 
 Personagem::Personagem(){
     this->vida = 100;
-    this->nome = ""; 
+    this->nome = "";
 }
+
 Personagem::~Personagem(){
-    cout << "Personagem Destruido" << endl;
-
+    cout << "Personagem destruido" << endl;
 }
-void Personagem::obterVida(){
+
+void Personagem::definirVida(int v){
+    this->vida = v;
+}
+
+int Personagem::obterVida(){
     return this->vida;
-
 }
-void Personagem::obterNome(){
+
+void Personagem::definirNome(string n){
+    this->nome = n;
+}
+
+string Personagem::obterNome(){
     return this->nome;
 }
+
 void Personagem::atacar(Personagem * p){
     cout << this->nome << "ataca" << p->obterNome() << endl;
     p->defender(10);
 }
+
 void Personagem::defender(int dano){
-    this->vida -=dano;
-    cout << this->nome << "defende" << dano <<"de dano" << endl;
-    p->defender(10);
+    this->vida -= dano;
+    cout << this->nome << "defende" << dano << "de dano" << endl;
 }
 void Personagem::curar(int cura){
     if(this->vida < 100){
-        if((100 - this->vida)>= cura)
-        this->vida +=cura;
+        if ((100 - this->vida) >= cura)
+            this->vida += cura;
+        else
+            this->vida = 100;
     }
-    cout << this->nome << "cura" << cura <<"se cura" << endl;
-    
+    cout << this->nome << "cura" << cura << "de vida" << endl;    
+}
+
+void Personagem::mostrarStatus(){
+    cout << "Nome: " << this->nome << endl;
+    cout << "Vida: " << this->vida << endl;
 }
