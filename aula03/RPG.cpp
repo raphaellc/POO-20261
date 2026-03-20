d7b7cc1d5bccddcc8f5e84bd9018287b80a640e5
Niwlek97 <kelwincleyton@gmail.com>
2026-03-20 19:39:57 -0300

    RPG.cpp

--- aula03/RPG.cpp
+++ aula03/RPG.cpp
@@ -1,12 +1,11 @@
-#include"RPG.h"
+#include "RPG.h"
 #include <iostream>
 using namespace std;
-
-RPG::RPG(Personagem*p1, Personagem *p2){
-    this-> personagem1 = p1;
-    this-> personagem2= p2;
+RPG::RPG(Personagem *p1, Personagem *p2){
+    this->personagem1 = p1;
+    this->personagem2 = p2;
 }
 
 void RPG::iniciarBatalha(){
-    cout << "Iniciando Batalha"
+    cout << "Iniciando batalha" << endl; 
 }
